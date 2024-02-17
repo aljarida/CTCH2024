1892ca6058c57e5360d373fab31df74c452ae0ab
aljarida <jarida@bu.edu>
2024-02-17 18:47:50 -0500

    add mongoose

--- client/app/page.js
+++ client/app/page.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 export default function Home() {
 
   const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
@@ -12,14 +13,24 @@ export default function Home() {
     ).then(
       data => {
         console.log(data)
-        setMessage(data.message);
+        setMessage(data.message)
+        setPeople(data.people)
       }
     )
   }, []);
 
   return (
     <main>
-      {message}
+      <div>{message}</div>
+      <div>
+        {
+          people.map((person, index) => (
+            <div key={index}>
+              {person}
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
 const cors = require("cors");
 const PORT = 8080;
 
-
 app.use(cors()); // middleware for accepting requests from front-end
 
+mongoose.connect(
+  process.env.MONGODB_URI, 
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  }
+);
+
 app.get("/api/home", (req, res) => {
-  res.json({ message: "Hey, mate!" });
+  res.json({ message: "Hey, mate!", people: ["Marcus", "Aurelius", "Luther", "King", "Jason", "Clark"] });
 })
 
 app.listen(PORT, () => {
